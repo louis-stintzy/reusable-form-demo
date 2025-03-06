07bccacae28e6e00a9a5be91830dc216bbce57fe
louis-stintzy <louis.stintzy@gmail.com>
2025-03-06 03:20:20 +0100

    refactor(form): create ReusableForm component and integrate with signup form

--- src/components/PageComponents/SignupForm/SignupForm.tsx
+++ src/components/PageComponents/SignupForm/SignupForm.tsx
@@ -1,19 +1,9 @@
-import { useForm, SubmitHandler } from 'react-hook-form';
-import { RegisterCredentials } from '../../../@types/auth';
-import { zodResolver } from '@hookform/resolvers/zod';
 import { signupFormSchema } from './signupForm.schema';
 import { signupFormConfig } from './signupForm.config';
-// import FormInput from '../../Common/ReusableForm/SubComponents/FormInput';
-import FormSubmitButton from '../../Common/ReusableForm/SubComponents/FormSubmitButton';
-import FormTitle from '../../Common/ReusableForm/SubComponents/FormTitle';
+import { RegisterCredentials } from '../../../@types/auth';
+import ReusableForm from '../../Common/ReusableForm/ReusableForm';
 
 import './SignupForm.style.css';
-import { formatTitle } from '../../../utils/formatTitle';
-import FormFooter from '../../Common/ReusableForm/SubComponents/FormFooter';
-import { useEffect, useState } from 'react';
-import FormBase from '../../Common/ReusableForm/SubComponents/FormBase';
-import FormInputs from '../../Common/ReusableForm/SubComponents/FormInputs';
-import FormContainer from '../../Common/ReusableForm/SubComponents/FormContainer';
 
 /*
  * Composant SignupForm : Formulaire d'inscription
@@ -24,96 +14,11 @@ import FormContainer from '../../Common/ReusableForm/SubComponents/FormContainer
  */
 
 function SignupForm() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<RegisterCredentials>({
-    resolver: zodResolver(signupFormSchema),
-  });
-
-  const [footerMessage, setFooterMessage] = useState<{
-    type: 'error' | 'success' | 'default';
-    content: {
-      text: string;
-      linkText: string;
-      linkTo: string;
-    };
-  } | null>(null);
-  const title = signupFormConfig.title;
-  const formattedTitle = formatTitle(title);
-
-  // note : Simulation de l'état de chargement, de succès et d'erreur
-  const isloading = false;
-
-  const footerError = {
-    text: 'Email already exists,',
-    linkText: 'please login ',
-    linkTo: '/login',
-  };
-  const footerSuccess = {
-    text: 'Account created successfully',
-    linkText: 'Login now',
-    linkTo: '/login',
-  };
-
-  const onSubmit: SubmitHandler<RegisterCredentials> = (data) => {
-    console.log('Données du formulaire', data);
-    // note : ici, on simule un appel API pour vérifier si l'email existe déjà
-    if (data.email === 'bad@mail.com') {
-      setFooterMessage({
-        type: 'error',
-        content: footerError,
-      });
-    }
-    if (data.email === 'good@mail.com') {
-      setFooterMessage({
-        type: 'success',
-        content: footerSuccess,
-      });
-    }
-  };
-
-  const [animationKey, setAnimationKey] = useState(0);
-
-  useEffect(() => {
-    // Change la clé à chaque changement de message, pour forcer le re-rendu
-    setAnimationKey((prev) => prev + 1);
-  }, [footerMessage]);
-
   return (
-    <FormContainer formattedTitle={formattedTitle}>
-      <FormTitle formattedTitle={formattedTitle} title={title} />
-      <FormBase
-        formattedTitle={formattedTitle}
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <FormInputs<RegisterCredentials>
-          formattedTitle={formattedTitle}
-          fields={signupFormConfig.fields}
-          errors={errors}
-          register={register}
-        />
-        <FormSubmitButton
-          formattedTitle={formattedTitle}
-          isLoading={isloading}
-          buttonText={signupFormConfig.buttonText}
-        />
-      </FormBase>
-      <FormFooter
-        key={animationKey}
-        formattedTitle={formattedTitle}
-        type={footerMessage?.type ?? 'default'}
-        footerLink={
-          footerMessage?.content ??
-          signupFormConfig?.footerLink ?? {
-            text: '',
-            linkText: '',
-            linkTo: '',
-          }
-        }
-      />
-    </FormContainer>
+    <ReusableForm<RegisterCredentials>
+      formConfig={signupFormConfig}
+      formSchema={signupFormSchema}
+    />
   );
 }
 
