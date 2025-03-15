bc5e746749b5f19ffba7985540af9f11d8e35e17
louis-stintzy <louis.stintzy@gmail.com>
2025-03-15 22:48:13 +0100

    refactor(demo): typing travelClasses and tripTypes for use in the validation schema(FlightReservationForm)

--- src/components/PageComponents/FormDemo/FlightReservationForm/flightReservationForm.schema.ts
+++ src/components/PageComponents/FormDemo/FlightReservationForm/flightReservationForm.schema.ts
@@ -1,15 +1,25 @@
 import { z } from 'zod';
+import {
+  travelClassesValuesTuple,
+  tripTypesValuesTuple,
+} from './fligthReservationForm.constant';
 
 export const flightReservationFormSchema = z
   .object({
-    tripType: z.enum(['RoundTrip', 'OneWay']),
+    tripType: z.enum(tripTypesValuesTuple),
     departureAirport: z.string().min(1, 'Veuillez saisir une ville de départ.'),
     arrivalAirport: z.string().min(1, "Veuillez saisir une ville d'arrivée."),
-    departureDate: z.string(),
+    departureDate: z
+      .string()
+      .min(1, 'Veuillez saisir une date de départ.')
+      .refine((date) => {
+        const today = new Date().toISOString().split('T')[0];
+        return date >= today;
+      }, 'La date de départ doit être ultérieure à aujourd’hui.'),
     returnDate: z.string().optional(),
     adults: z.number().min(1, 'Au moins 1 adulte requis.'),
     children: z.number().min(0, 'Nombre invalide.'),
-    travelClass: z.enum(['Economy', 'Premium', 'Business', 'LaPremière']),
+    travelClass: z.enum(travelClassesValuesTuple),
     useBluebiz: z.boolean(),
   })
   .refine((data) => data.tripType === 'OneWay' || !!data.returnDate, {
--- src/components/PageComponents/FormDemo/FlightReservationForm/fligthReservationForm.constant.ts
+++ src/components/PageComponents/FormDemo/FlightReservationForm/fligthReservationForm.constant.ts
@@ -1,4 +1,9 @@
-export const travelClasses = [
+export const travelClasses: [
+  Record<'value' | 'label', string>,
+  Record<'value' | 'label', string>,
+  Record<'value' | 'label', string>,
+  Record<'value' | 'label', string>,
+] = [
   { value: 'Economy', label: 'Economy' },
   { value: 'Premium', label: 'Premium' },
   { value: 'Business', label: 'Business' },
@@ -7,9 +12,16 @@ export const travelClasses = [
 
 export const travelClassesValuesTuple = travelClasses.map(
   (travelClass) => travelClass.value
-);
+) as [string, ...string[]]; // as [string, string, string, string] // typage pour zod
 
-export const tripTypes = [
+export const tripTypes: [
+  Record<'value' | 'label', string>,
+  Record<'value' | 'label', string>,
+] = [
   { value: 'RoundTrip', label: 'Aller-retour' },
   { value: 'OneWay', label: 'Aller simple' },
 ];
+
+export const tripTypesValuesTuple = tripTypes.map(
+  (tripType) => tripType.value
+) as [string, string]; // typage pour zod
