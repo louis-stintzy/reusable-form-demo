e4cc98b46423703501fba47c72383736406fc332
louis-stintzy <louis.stintzy@gmail.com>
2025-03-17 21:44:28 +0100

    feat(landing-page): Navbar (mobile drawer)

--- src/components/Common/Navbar/Navbar.tsx
+++ src/components/Common/Navbar/Navbar.tsx
@@ -2,8 +2,15 @@ import { NavLink } from 'react-router-dom';
 
 import './Navbar.css';
 import logo from '../../../assets/logo2.png';
+import menu from '../../../assets/menu.svg';
+import X from '../../../assets/x.svg';
+import { useState } from 'react';
 
 function Navbar() {
+  const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
+  const toggleNavbar = () => {
+    setMobileDrawerOpen(!mobileDrawerOpen);
+  };
   const navLinks = [
     { title: 'Features', path: '#' },
     // { title: 'Workflow', path: '#' },
@@ -42,6 +49,46 @@ function Navbar() {
             </NavLink>
           </li>
         </ul>
+        {/* Mobile Menu Button */}
+        <div className="mobile-menu-button">
+          <button onClick={toggleNavbar}>
+            {mobileDrawerOpen ? (
+              <img src={X} alt="close" />
+            ) : (
+              <img src={menu} alt="menu" />
+            )}
+          </button>
+        </div>
+        {/* Mobile Drawer */}
+        {mobileDrawerOpen && (
+          <div className="mobile-menu">
+            <ul className="mobile-nav-links">
+              {navLinks.map((item, index) => (
+                <li key={index} className="mobile-nav-item">
+                  <a href={item.path}>{item.title}</a>
+                </li>
+              ))}
+              <li className="mobile-nav-item">
+                <NavLink to="/login" className="mobile-signin">
+                  Sign In
+                </NavLink>
+              </li>
+              <li className="mobile-nav-item">
+                <NavLink to="/signup" className="mobile-create-account">
+                  Create an accunt
+                </NavLink>
+              </li>
+            </ul>
+            {/* <div className="mobile-buttons">
+              <a href="#" className="mobile-login-button">
+                Log in
+              </a>
+              <a href="#" className="mobile-create-account">
+                Sign up
+              </a>
+            </div> */}
+          </div>
+        )}
         {/* ----- */}
       </div>
     </nav>
