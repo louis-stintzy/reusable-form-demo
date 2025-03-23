80fc0cc950c9420d462d5875bfdfda1c3a5ff6f1
louis-stintzy <louis.stintzy@gmail.com>
2025-03-23 04:16:36 +0100

    feat(auth): install axios + update authService

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,5 +1,7 @@
-import { LoginCredentials } from '../@types/auth';
+import { LoginCredentials, RegisterCredentials } from '../@types/auth';
+import { axiosInstance } from './axiosInstance';
 
+// TODO reprendre et uniformiser les types frontend et backend
 export interface UserData {
   id: number;
   name: string;
@@ -7,24 +9,48 @@ export interface UserData {
   role: 'admin' | 'user';
 }
 
+export const registerUser = async (
+  credentials: RegisterCredentials
+): Promise<UserData> => {
+  const response = await axiosInstance.post<UserData>(
+    '/auth/register',
+    credentials
+  );
+  return response.data;
+};
+
 export const loginUser = async (
   credentials: LoginCredentials
 ): Promise<UserData> => {
-  // Simulate API call
-  return new Promise((resolve, reject) =>
-    setTimeout(() => {
-      if (
-        credentials.email === 'good@mail.com' &&
-        credentials.password === '12345678910Ll$'
-      ) {
-        resolve({
-          id: 1,
-          name: 'John Doe',
-          email: credentials.email,
-          role: 'user',
-        });
-      }
-      reject(new Error('Invalid credentials'));
-    }, 300)
+  const response = await axiosInstance.post<UserData>(
+    '/auth/login',
+    credentials
   );
+  return response.data;
+  // Simulate API call
+  // return new Promise((resolve, reject) =>
+  //   setTimeout(() => {
+  //     if (
+  //       credentials.email === 'good@mail.com' &&
+  //       credentials.password === '12345678910Ll$'
+  //     ) {
+  //       resolve({
+  //         id: 1,
+  //         name: 'John Doe',
+  //         email: credentials.email,
+  //         role: 'user',
+  //       });
+  //     }
+  //     reject(new Error('Invalid credentials'));
+  //   }, 300)
+  // );
+};
+
+export const logoutUser = async (): Promise<void> => {
+  await axiosInstance.post('/auth/logout');
+};
+
+export const verifyToken = async (): Promise<UserData> => {
+  const response = await axiosInstance.post<UserData>('/auth/verify');
+  return response.data;
 };
